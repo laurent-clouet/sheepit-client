90362df266ecb8ba872ec140259997a9c26b7d44
Mathis Chenuet <artdevelopp@hotmail.fr>
2015-01-25 18:36:39 +0000

    Cleanup: Use foreach whenever is possible

--- src/com/sheepit/client/Configuration.java
+++ src/com/sheepit/client/Configuration.java
@@ -197,8 +197,7 @@ public class Configuration {
 		
 		File[] files = dir.listFiles();
 		if (files != null) {
-			for (int i = 0; i < files.length; i++) {
-				File file = files[i];
+			for (File file : files) {
 				if (file.isDirectory()) {
 					Utils.delete(file);
 				}
