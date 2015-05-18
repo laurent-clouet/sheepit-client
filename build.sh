feffea277e9882efe4bf1eb029a690cf9f317761
Laurent Clouet <github@nopnop.net>
2015-05-18 21:47:26 +0100

    Update openjdk file

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+jvm_name="openjdk-1.7.0-u80-unofficial-windows-amd64-image"
 mkdir jre
-wget https://bitbucket.org/alexkasko/openjdk-unofficial-builds/downloads/openjdk-1.7.0-u60-unofficial-windows-amd64-image.zip
-unzip openjdk-1.7.0-u60-unofficial-windows-amd64-image.zip
-cp -rf openjdk-1.7.0-u60-unofficial-windows-amd64-image/* jre/
+wget https://bitbucket.org/alexkasko/openjdk-unofficial-builds/downloads/$jvm_name.zip
+unzip $jvm_name.zip
+cp -rf $jvm_name/* jre/
 rm -rf jre/include jre/src.zip
 wget https://www.sheepit-renderfarm.com/media/applet/client-latest.php -O jre/sheepit-client.jar
 cd jre
 7z a ../application.7z .
 cd ..
 cat starter.sfx config.cfg application.7z > sheepit-wrapper.exe
-rm -rf application.7z jre  openjdk-1.7.0-u60-unofficial-windows-amd64-image openjdk-1.7.0-u60-unofficial-windows-amd64-image.zip
+rm -rf application.7z jre  $jvm_name $jvm_name.zip
