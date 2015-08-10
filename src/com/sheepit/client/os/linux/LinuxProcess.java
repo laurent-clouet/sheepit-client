b0981003b142a4863c13bd2884bd19a6546027cd
hailIulius <gavrilov.iulian@gmail.com>
2015-08-10 13:25:20 +0100

    Improvements to Linux affinity handling

--- src/com/sheepit/client/os/linux/LinuxProcess.java
+++ src/com/sheepit/client/os/linux/LinuxProcess.java
@@ -13,20 +13,19 @@ import com.sun.jna.ptr.PointerByReference;
 public class LinuxProcess {
     
     public static boolean setAffinity(int pid, long affinity) {
-        BitSet bs = Utils.convertLongToBitset64(affinity);
-        System.out.println("setAffinity for pid:"+pid+" affinity"+affinity);
+        //System.out.println("setAffinity for pid:"+pid+" affinity"+affinity);
         if (pid == -1)
             return false;
-        if (bs.isEmpty())
+        if (affinity == 0) {
             return false;
+        }
         try {
-            byte[] buff = bs.toByteArray();
-            int cpuSetSizeInBytes = buff.length;
-            Memory cpusetArray = new Memory(cpuSetSizeInBytes);
-            cpusetArray.write(0, buff, 0, buff.length);
-            return CLibrary.INSTANCE.sched_setaffinity(pid, cpuSetSizeInBytes, new PointerByReference(cpusetArray)) == 0;
+            long affinityMask[] = new long[16];
+            affinityMask[0] = affinity;
+            final int result = CLibrary.INSTANCE.sched_setaffinity(0, 16 * (Long.SIZE / 8), affinityMask);
+            return (result == 0);
         } catch (Exception e) {
-            System.err.println("setAffinity error for pid:"+pid+" affinity"+affinity + " " + e);
+            System.err.println("setAffinity error for pid:" + pid + " affinity" + affinity + " " + e);
             return false;
         }
     }
@@ -48,6 +47,6 @@ public class LinuxProcess {
 
         int sched_setaffinity(final int pid,
                               final int cpusetsize,
-                              final PointerType cpuset);
+                              final long[] cpuset);
     }
 }
