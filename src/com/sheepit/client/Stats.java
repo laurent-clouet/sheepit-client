f288b69aba7d3c930bda83ad314280ba703c21d5
Laurent Clouet <github@nopnop.net>
2016-10-12 00:34:51 +0200

    * Better ui with more infos * Remove revision from protocol since it's not use anymore

---
+++ src/com/sheepit/client/Stats.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2016 Laurent CLOUET
+ * Author Laurent CLOUET <laurent.clouet@nopnop.net>
+ *
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+ 
+package com.sheepit.client;
+
+public class Stats {
+	private int remainingFrame;
+	private int creditsEarned;
+	private int creditsEarnedSession;
+	private int waitingProject;
+	private int connectedMachine;
+	
+	public Stats(int frame, int credits, int creditsSession, int waitings, int machines) {
+		remainingFrame = frame;
+		creditsEarned = credits;
+		creditsEarnedSession = creditsSession;
+		waitingProject = waitings;
+		connectedMachine = machines;
+	}
+	
+	public Stats() {
+		remainingFrame = 0;
+		creditsEarned = 0;
+		creditsEarnedSession = 0;
+		waitingProject = 0;
+		connectedMachine = 0;
+	}
+
+	public int getRemainingFrame() {
+		return remainingFrame;
+	}
+	
+	public int getCreditsEarnedDuringSession() {
+		return creditsEarnedSession;
+	}
+	
+	public int getCreditsEarned() {
+		return creditsEarned;
+	}
+	
+	public int getWaitingProject() {
+		return waitingProject;
+	}
+	
+	public int getConnectedMachine() {
+		return connectedMachine;
+	}
+
+	@Override
+	public String toString() {
+		return "Stats [remainingFrame=" + remainingFrame + ", creditsEarned=" + creditsEarned + ", creditsEarnedSession=" + creditsEarnedSession + ", waitingProject=" + waitingProject + ", connectedMachine=" + connectedMachine + "]";
+	}
+}
