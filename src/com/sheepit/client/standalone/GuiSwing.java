ac08081fd9b4410e55594846cd63183e9a550ea8
Laurent Clouet <github@nopnop.net>
2017-04-30 23:08:04 +0200

    Increase size of windows since the settings screen display more information

--- src/com/sheepit/client/standalone/GuiSwing.java
+++ src/com/sheepit/client/standalone/GuiSwing.java
@@ -119,7 +119,7 @@ public class GuiSwing extends JFrame implements Gui {
 		}
 		
 		setTitle("SheepIt Render Farm");
-		setSize(520, 640);
+		setSize(520, 660);
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
