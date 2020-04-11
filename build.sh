652533c1145e4b9a73a2f40cdaabfdd3db109e0e
Laurent Clouet <laurent@nopnop.net>
2020-04-11 10:52:08 +0200

    Update to a more recent jre

--- build.sh
+++ build.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-jvm_name="openjdk-1.7.0-u80-unofficial-windows-amd64-image"
+jvm_name="jdk-11.0.6+10-jre"
+
+#creating folder structure
 mkdir jre
-wget https://bitbucket.org/alexkasko/openjdk-unofficial-builds/downloads/$jvm_name.zip
-unzip $jvm_name.zip
-cp -rf $jvm_name/* jre/
-rm -rf jre/include jre/src.zip
+mkdir jre/jre
+
+#Pull latest client
 wget https://www.sheepit-renderfarm.com/media/applet/client-latest.php -O jre/sheepit-client.jar
+
+#unzip jre
+unzip $jvm_name.zip
+cp -rf $jvm_name/* jre/jre/
+
+#Building the exe bundle
 cd jre
 7z a ../application.7z .
 cd ..
 cat starter.sfx config.cfg application.7z > sheepit-wrapper.exe
-rm -rf application.7z jre  $jvm_name $jvm_name.zip
+
+#Cleanup
+rm -rf application.7z jre  $jvm_name
