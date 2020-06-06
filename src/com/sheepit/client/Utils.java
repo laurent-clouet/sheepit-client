bb67c18fa6c5d4580b5c6da3e238697d2ebee7a9
Luis Uguina <luguina@users.noreply.github.com>
2020-06-06 18:26:33 +1000

    Refactor: remove unused imports (#248)

--- src/com/sheepit/client/Utils.java
+++ src/com/sheepit/client/Utils.java
@@ -20,8 +20,6 @@
 package com.sheepit.client;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
