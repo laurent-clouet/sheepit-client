d318fa5f0e2ef50c9b398c856e05621379bdc0ba
tuxun <tuxunpro@gmail.com>
2020-07-28 15:28:06 +0200

    restore missing time remaining time in UI with openCL (#282)
    * restore missing time remaining time in UI with openCL

--- src/com/sheepit/client/Job.java
+++ src/com/sheepit/client/Job.java
@@ -211,7 +211,6 @@ import lombok.Getter;
 		if (isUseGPU() && configuration.getGPUDevice() != null && configuration.getComputeMethod() != ComputeType.CPU && OpenCL.TYPE
 				.equals(configuration.getGPUDevice().getType())) {
 			new_env.put("CYCLES_OPENCL_SPLIT_KERNEL_TEST", "1");
-			this.updateRenderingStatusMethod = UPDATE_METHOD_BY_TILE; // don't display remaining time
 		}
 		
 		for (String arg : command1) {
